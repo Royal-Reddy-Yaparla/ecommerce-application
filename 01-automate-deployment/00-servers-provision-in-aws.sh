5299fa0974b1bfd3394610d0038ecf1d8eab7db1
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-05-20 16:39:49 +0530

    servers-provision

--- 01-automate-deployment/00-servers-provision-in-aws.sh
+++ 01-automate-deployment/00-servers-provision-in-aws.sh
@@ -13,12 +13,19 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+USER_ID=$(id -u)
+
 # logs setup
 LOG_REPO="/var/log/servers-provison"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOG_REPO/$SCRIPT_NAME.log"
 # echo "$LOG_FILE"
 
+if [ $USER_ID -ne 0 ]
+then
+    echo -e "$R ERROR: need to provide sudo user access $N"
+    exit 1
+fi
 
 # Validate command
 VALIDATE(){
