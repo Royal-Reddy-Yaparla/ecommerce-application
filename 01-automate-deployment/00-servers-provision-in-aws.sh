ba0a372a3ca6b39acccd5ce57fc09cefa4201a99
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-05-21 06:58:21 +0530

    servers-provision

--- 01-automate-deployment/00-servers-provision-in-aws.sh
+++ 01-automate-deployment/00-servers-provision-in-aws.sh
@@ -26,6 +26,7 @@ SECURITY_GR_ID="sg-0c07dddd955fb376a"
 SUBNET_ID="subnet-0b2ada4cfcc744c81"
 INSTANCE_TYPE="t2.micro"
 ZONE_ID="Z07146881RI4INQX613W7"
+DOMAIN_NAME="royalreddy.site"
 
 
 echo -e "script is started execution at $G $(date) $N"  | tee -a $LOG_FILE
@@ -76,20 +77,19 @@ aws route53 change-resource-record-sets \
   --hosted-zone-id $ZONE_ID \
   --change-batch '
   {
-    "Comment": "Create a new A record "
-    ,"Changes": [{
-    "Action"            : "UPSERT"
-    ,"ResourceRecordSet": {
-        "Name"          : "royalreddy.site"
-        ,"Type"         : "A"
-        ,"TTL"          : 1,
-        ,"ResourceRecords": [{ 
-                "Value"     : "'$PUBLIC_IP'"
+        "Comment": "Create a new A record "
+        ,"Changes": [{
+        "Action"            : "UPSERT"
+        ,"ResourceRecordSet": {
+            "Name"          : "'$DOMAIN_NAME'"
+            ,"Type"         : "A"
+            ,"TTL"          : 1,
+            ,"ResourceRecords": [{ 
+                    "Value"     : "'$PUBLIC_IP'"
+            }]
+        }
         }]
-    }
- }]
-}
-'
+}'
 # INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GR_ID --subnet-id $SUBNET_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=test}]' --query 'Instances[*].InstanceId' --output text)
 
 # echo "$INSTANCE_ID"
