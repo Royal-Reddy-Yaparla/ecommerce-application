4c03684ca50f76a51bdbab99a398299a83860bf9
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-05-21 10:41:22 +0530

    servers-provision

--- 01-automate-deployment/00-servers-provision-in-aws.sh
+++ 01-automate-deployment/00-servers-provision-in-aws.sh
@@ -80,19 +80,20 @@ do
     echo "$RECORD_NAME"
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
-    --change-batch '{
-            "Comment": "Create a new A record",
-            "Changes": [{
-                "Action"            : "UPSERT",
-                "ResourceRecordSet": {
-                    "Name"          : "'"$RECORD_NAME"'",
-                    "Type"         : "A",
-                    "TTL"          : 1,
-                    "ResourceRecords": [{ 
-                            "Value"     : "'$IP'"
-                    }]
-                }
+    --change-batch '
+    {
+        "Comment": "Creating or Updating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
             }]
+        }
+        }]
     }'
 done
 
