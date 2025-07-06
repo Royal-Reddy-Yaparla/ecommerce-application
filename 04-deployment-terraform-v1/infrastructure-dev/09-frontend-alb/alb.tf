ada3648707e092a28ea848360df07f9eff58c3d6
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2025-07-06 07:33:07 +0530

    frontend alb https listener added

--- 04-deployment-terraform-v1/infrastructure-dev/09-frontend-alb/alb.tf
+++ 04-deployment-terraform-v1/infrastructure-dev/09-frontend-alb/alb.tf
@@ -17,26 +17,10 @@ module "alb" {
   )
 }
 
-resource "aws_lb_listener" "frontend_http" {
-  load_balancer_arn = module.alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/html"
-      message_body = "Hello I am Frontend Load-Balancer"
-      status_code  = "200"
-    }
-  }
-}
-
-# resource "aws_lb_listener" "frontend_https" {
+# resource "aws_lb_listener" "frontend_http" {
 #   load_balancer_arn = module.alb.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
+#   port              = "80"
+#   protocol          = "HTTP"
 
 #   default_action {
 #     type = "fixed-response"
@@ -49,3 +33,30 @@ resource "aws_lb_listener" "frontend_http" {
 #   }
 # }
 
+resource "aws_lb_listener" "frontend_https" {
+  load_balancer_arn = module.alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = local.certificate_arn
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
+      message_body = "Hello I am Frontend Load-Balancer"
+      status_code  = "200"
+    }
+  }
+}
+
+resource "aws_route53_record" "backend-alb" {
+  zone_id = var.zone_id
+  name = "${var.environment}.${var.domain}"
+  type = "A"
+  alias {
+    name                   = module.alb.dns_name
+    zone_id                = module.alb.zone_id
+    evaluate_target_health = true
+  }
+}
--- 04-deployment-terraform-v1/infrastructure-dev/09-frontend-alb/locals.tf
+++ 04-deployment-terraform-v1/infrastructure-dev/09-frontend-alb/locals.tf
@@ -2,6 +2,7 @@ locals {
   vpc_id             = data.aws_ssm_parameter.vpc.value
   public_subnet_ids  = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
   security_group_ids = ["${data.aws_ssm_parameter.frontend_alb_sg_id.value}"]
+  certificate_arn = data.aws_ssm_parameter.aws_acm_certificate_arn.value
   common_tags = {
     Project     = var.project
     Environment = var.environment
